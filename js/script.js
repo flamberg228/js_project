01da9ed6c6496f5b467d7b2ca618c3ca31374792
Artem Pavlioglo <okz21_pa@ukr.net>
2020-01-24 13:30:33 +0300

    выполнил обычное задание второго урока

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-let money, income, addExpenses, deposit, mission, period;
+let money, income, addExpenses, deposit, mission, period, budgetDay;
+money = 10000; // доход за месяц 
+income = 'фриланс';
+addExpenses = 'интернет, комуналка, телефон';
+deposit = true;
+mission = 20000; // цель
+period = 2; // время достижения цели
+budgetDay = money / 30; // дневной доход
 
-alert('Работает?');
-console.log('Проверка');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' ' + 'месяцам');
+console.log('Цель заработать ' + mission + ' ' + 'гривен');
+console.log('Дневной доход равен = ' + Math.trunc(budgetDay) + ' ' + 'гривны');
