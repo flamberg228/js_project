52d79627f80c94cb4ef85ce8ceaf909f3d2a9949
Artem Pavlioglo <okz21_pa@ukr.net>
2020-01-28 14:23:51 +0300

    dd

--- js/script.js
+++ js/script.js
@@ -1,40 +1,36 @@
 'use strict';
-let money, income, addExpenses, deposit, mission, period, budgetDay;
-money = 10000; // доход за месяц 
+let money, income, addExpenses, deposit, mission, budgetDay, period;
+// money = 10000; // доход за месяц 
 income = 'фриланс';
-addExpenses = 'Интернет, Комуналка, Телефон';
+// addExpenses = 'Интернет, Комуналка, Телефон';
 deposit = true;
 mission = 20000; // цель
 period = 2; // время достижения цели
 budgetDay = money / 30; // дневной доход
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' ' + 'месяцам');
-console.log('Цель заработать ' + mission + ' ' + 'гривен');
-let mas = (addExpenses.toLowerCase());
+let getTypeOf = function (name) {
+  let type = typeof(name);
+  return type;
+} 
+ console.log(getTypeOf(money));
+ console.log(getTypeOf(income));
+ console.log(getTypeOf(deposit));
 
-console.log(mas.split(', '));
-console.log('Дневной доход равен = ' + Math.trunc(budgetDay) + ' ' + 'гривны');
+
+// console.log(addExpenses.length);
+// console.log('Период равен ' + period + ' ' + 'месяцам');
+// console.log('Цель заработать ' + mission + ' ' + 'гривен');
+
+// console.log('Дневной доход равен = ' + Math.trunc(budgetDay) + ' ' + 'гривны');
 
 money = +prompt('Ваш месячный доход?');
 
 addExpenses = String(prompt('Перечислите выши расходы через запятую'));
-deposit = confirm('Есть ли у вас депозит?');
-console.log(deposit);
-// let t, f;
-// f = 'нет';
-// t = 'да';
+let mas = (addExpenses.toLowerCase());
+console.log(mas.split(', '));
 
-// if (deposit === t) {
-//   deposit=true;
-//   console.log(deposit);
-// } else if (deposit === f) {
-//   deposit=false;
-//   console.log(deposit);
-// }
+deposit = confirm('Есть ли у вас депозит?');
+// console.log(deposit);
 
 let expenses1 = String(prompt('Введите обязательную статью расходов?'));
 let amount1 = +prompt('Во сколько это обойдется?');
@@ -42,25 +38,45 @@ let amount1 = +prompt('Во сколько это обойдется?');
 let expenses2 = String(prompt('Введите обязательную статью расходов?[2]'));
 let amount2 = +prompt('Во сколько это обойдется?[2]');
 
-let budgetMonth = Math.ceil(money - amount1 - amount2);
-console.log('Месячный бюджет равен: ' + budgetMonth);
+let getExpensesMonth = function () {
+  let sum = amount1 + amount2;
+  return sum;
+}
+console.log('Сумма обязательных расходов равна: ' + getExpensesMonth());
 
-period = mission/budgetMonth;
-console.log('Цель будет достигнута за ' + period.toFixed(1) + ' месяцев');
+function getAccumulatedMonth () {
+  let budgetMonth = Math.ceil(money - amount1 - amount2);
+  return budgetMonth;
+}
 
-budgetDay = Math.floor(budgetMonth/30);
+let accumulatedMonth = getAccumulatedMonth();
+
+console.log('Месячный бюджет равен: ' + accumulatedMonth);
+
+let getTargetMonth = function () {
+  let duration = mission/accumulatedMonth;
+  return duration;
+}
+
+console.log('Цель будет достигнута за ' + getTargetMonth().toFixed(1) + ' месяцев');
+
+budgetDay = Math.floor(accumulatedMonth/30);
 console.log('Бюджет на день равен: ' + budgetDay);
 
+let getStatusIncome = function() {
 if (budgetDay >= 1200) {
-  console.log('У вас высокий уровень дохода')
+  return ('У вас высокий уровень дохода');
 } else if (budgetDay < 1200 && budgetDay >= 600) {
-  console.log('У вас средний уровень дохода')
+  return ('У вас средний уровень дохода');
 } else if (budgetDay < 600 && budgetDay > 0) {
-  console.log('К сожалению у вас уровень дохода ниже среднего')
+  return ('К сожалению у вас уровень дохода ниже среднего');
 } else if (budgetDay === 0) {
-  console.log('Ваш доход равен 0. Поздравляю, вы безработный!')
+  return ('Ваш доход равен 0. Поздравляю, вы безработный!');
 } else if (budgetDay < 0) {
-  console.log('Вы ввели отрицательное значение')
+  return ('Вы ввели отрицательное значение');
 } else {
-  console.log('Вы ввели некорректное значение')
+  return ('Вы ввели некорректное значение');
 }
+}
+
+console.log(getStatusIncome());
