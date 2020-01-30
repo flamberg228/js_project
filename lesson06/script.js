7f23fc9edff82e6ea3b36441eca77cf60c4f1d78
Artem Pavlioglo <okz21_pa@ukr.net>
2020-01-30 22:26:16 +0300

    добавил замыкание

--- lesson06/script.js
+++ lesson06/script.js
@@ -6,8 +6,13 @@ function game () {
   //   return num;
   // // }
   // let clc = number();
+  let num;
+  function one() {
   let num = Math.floor(Math.random() * Math.floor(100)) + 1;
   console.log(num);
+  return num;
+  } 
+  let two = one();
   function check () {
  
   let ask = prompt('Угадай число от 1 до 100 ');
@@ -21,13 +26,14 @@ function game () {
   } else if(ask === null) {
     alert('Игра закончена')
   } 
-  else if(ask<num) {
+  else if(ask<two) {
+    
     alert('Загаданное число больше')
     check();
-  } else if(ask>num) {
+  } else if(ask>two) {
     alert('Загаданное число меньше')
     check();
-  } else if (ask == num) {
+  } else if (ask == two) {
     let yes = alert('Вы угадали ');
     return;
     // if(yes === true) {
