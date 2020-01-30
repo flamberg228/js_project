2bcf35490127a475f5da06b7ebad3813c2c2e5aa
Artem Pavlioglo <okz21_pa@ukr.net>
2020-01-30 18:21:02 +0300

    исправил

--- lesson06/script.js
+++ lesson06/script.js
@@ -3,31 +3,36 @@
 function check() {
   let num = 1;
   function number () {
-    num = 78;
+    num = Math.floor(Math.random() * Math.floor(100)) + 1;
     return num;
   }
+  console.log(number())
 
   
-  let ask = prompt('Угадай число от 1 до 100 ').trim();
+  let ask = prompt('Угадай число от 1 до 100 ');
+  console.log(ask)
   // let counter = 1; 
   
   // console.log(counter)
   if(isNaN(ask)  || ask === '') {
     alert('Введите число');
     check();
-  } else if(ask<number()) {
-    confirm('Заданное число меньше')
+  } else if(ask === null) {
+    alert('Игра закончена')
+  } 
+  else if(ask<number()) {
+    alert('Заданное число меньше')
     check();
   } else if(ask>number()) {
-    confirm('Заданное число больше')
+    alert('Заданное число больше')
     check();
   } else if (ask == number()) {
     let y = confirm('Вы угадали. Хотите сыграть еще раз? ');
     if(y===true) {
       check();
     }
+   
   }
-
 
 
 }
