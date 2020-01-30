7f7e8a95d4ef111c035d50f225ae668f120381cc
Artem Pavlioglo <okz21_pa@ukr.net>
2020-01-30 16:26:13 +0300

    исправил

--- lesson06/script.js
+++ lesson06/script.js
@@ -1,75 +1,36 @@
 'use strict';
 
-// let randomInteger = function (min, max) {
-//     return min + Math.random() * (max - min);
-// }
-
-let num =  78;
-
-let ask2;
 function check() {
+  let num = 1;
+  function number () {
+    num = 78;
+    return num;
+  }
+
+  
   let ask = prompt('Угадай число от 1 до 100 ').trim();
- 
+  // let counter = 1; 
+  
+  // console.log(counter)
   if(isNaN(ask)  || ask === '') {
     alert('Введите число');
     check();
-  } else if(ask<num) {
+  } else if(ask<number()) {
     confirm('Заданное число меньше')
     check();
-  } else if(ask>num) {
+  } else if(ask>number()) {
     confirm('Заданное число больше')
     check();
-  } else if (ask == num) {
+  } else if (ask == number()) {
     let y = confirm('Вы угадали. Хотите сыграть еще раз? ');
     if(y===true) {
       check();
     }
   }
-  
+
+
+
 }
+
 check()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let num = 78;
-// let ask = +prompt('Угадай число от одноого до 100: ')
-// console.log(typeof(ask))
-
-// if(!isNaN(ask==='' && isNaN(ask)) && ask !== null) {
-//   let ask2 = prompt('Введи число')
-  
-// } else if(!isNaN(ask) && ask !== null){
-//   if(ask < num) {
-//     confirm('Загаданное число больше')
-//     let ask2 = prompt('Введи число')
-//     if(isNaN(ask2)) {
-//       let ask2 = prompt('Введи число')
-//     }
-//   } else if(ask > num) {
-//     confirm('Загаданное число меньше')
-//     let ask2 = prompt('Введи число')
-//     if(isNaN(ask2)) {
-//       let ask2 = prompt('Введи число')
-//     }
-// }
-// } 
