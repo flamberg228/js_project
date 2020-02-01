5662617fa1fa021b220cd7d4142aef563e325ba2
Artem Pavlioglo <okz21_pa@ukr.net>
2020-02-01 22:57:24 +0300

    исправил

--- lesson07-complicated/script.js
+++ lesson07-complicated/script.js
@@ -1,14 +1,23 @@
 let myMas = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'.italics(), 'Воскресенье'.italics()];
 console.log(myMas[0])
 let now = new Date()
-let day = now.getDay();  
+let day = now.getDay();
+console.log(now.getDay())  
+day = 6;
 if(day === 0) {
-  day++;
+  day = day + 6;
 }
-day = day - 1;
+else if(day > 0) {
+  day = day - 1;
+}
+
 
 for(let i = 0; i<myMas.length; i++) {
-myMas[day] = myMas[day].bold();
-document.write('<span>' + myMas[i] + '</span>' + '<br>');
+  // if(day === 0) {
+  //   myMas[6] = myMas[6].bold();
+  // // }
+  // else 
+  myMas[day] = myMas[day].bold();
+  document.write('<span>' + myMas[i] + '</span>' + '<br>');
 }
 
