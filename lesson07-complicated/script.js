a3b5e6c40f756b3a3f8784408d4045c576a08606
Artem Pavlioglo <okz21_pa@ukr.net>
2020-02-01 22:30:17 +0300

    исправил усложненное

--- lesson07-complicated/script.js
+++ lesson07-complicated/script.js
@@ -1,11 +1,14 @@
 let myMas = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'.italics(), 'Воскресенье'.italics()];
+console.log(myMas[0])
 let now = new Date()
-let day = now.getDay();
-day--;
-console.log(day)
-for(let i = 0; i<myMas.length; i++) {
-document.write('<span>' + myMas[i] + '</span>' + '<br>');
-myMas[day] = myMas[day].bold();
+let day = now.getDay();  
+if(day === 0) {
+  day++;
+}
+day = day - 1;
 
+for(let i = 0; i<myMas.length; i++) {
+myMas[day] = myMas[day].bold();
+document.write('<span>' + myMas[i] + '</span>' + '<br>');
 }
 
